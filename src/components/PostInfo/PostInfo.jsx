a47dbca931e8f5df9d44e62e9dc61b4daf3bbd8f
BoGoBooM <myPikcha@gmail.com>
2025-04-11 13:32:03 +0300

    PostInfo was corrected

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -9,7 +9,7 @@ export const PostInfo = ({ post }) => (
 
       <p>
         {' Posted by  '}
-        <UserInfo user={post.user} key={post.user.id} />
+        <UserInfo user={post.user} />
       </p>
     </div>
 
