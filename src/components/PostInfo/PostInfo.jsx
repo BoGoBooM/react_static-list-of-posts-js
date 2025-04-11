2d54e50ecef7043aa2ca815e6a0a7ccda0092f2c
BoGoBooM <myPikcha@gmail.com>
2025-04-11 13:34:46 +0300

    PostInfo was corrected v2

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -14,9 +14,7 @@ export const PostInfo = ({ post }) => (
     </div>
 
     <p className="PostInfo__body">{post.body}</p>
-
     <hr />
-
     {post.comments.length > 0 ? (
       <CommentList comments={post.comments} />
     ) : (
